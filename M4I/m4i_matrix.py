494a4533ef24dcfee5a517f65c8a78017b1d7d0f
SEARA\liviu <learmeanu@gmail.com>
2024-02-09 20:24:27 +1300

    WV - EOD __setitem__ working

--- M4I/m4i_matrix.py
+++ M4I/m4i_matrix.py
@@ -53,8 +53,21 @@ class M4I_Matrix(DenseMatrix):
             if isinstance(row, slice) and isinstance(cols, slice):
                 if row.step or cols.step:
                     raise NotImplementedError("'step' in slice not implemented.")
-                return self.SubMatrix(row.start, row.stop - row.start + 1, cols.start, cols.stop - cols.start + 1)
+                return self.SubMatrix(row.start, row.stop - row.start + 1, 
+                                      cols.start, cols.stop - cols.start + 1)
             return super().__getitem__(row, cols)
+        else:
+            raise TypeError(f"{type(self).__name__} indices must be integers or slices, not {type(key).__name__}") # type: ignore
+        
+    def __setitem__(self, key, value):
+        if isinstance(key, tuple):
+            (row, cols) = key
+    #         if isinstance(row, slice) and isinstance(cols, slice):
+    #             if row.step or cols.step:
+    #                 raise NotImplementedError("'step' in slice not implemented.")
+    #             return self.SubMatrix(row.start, row.stop - row.start + 1, 
+    #                                   cols.start, cols.stop - cols.start + 1)
+            super().__setitem__(row, cols, value)
         else:
             raise TypeError(f"{type(self).__name__} indices must be integers or slices, not {type(key).__name__}") # type: ignore
         
